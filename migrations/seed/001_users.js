d67387722ac177725d81fdef57d8006ed5729beb
Julien Tant <julien@craftyx.fr>
2023-01-01 22:46:58 -0700

    convert tabs to space

--- migrations/seed/001_users.js
+++ migrations/seed/001_users.js
@@ -9,7 +9,7 @@ exports.seed = async function(knex) {
   await knex('users').del();
 
   await knex('users').insert([
-	{password: bcrypt.hashSync('Testing1!!', 10), email: "admin@example.com", name: "Admin", is_validated: true, member_level: 100},
+    {password: bcrypt.hashSync('Testing1!!', 10), email: "admin@example.com", name: "Admin", is_validated: true, member_level: 100},
     {password: bcrypt.hashSync('Testing1!!', 10), email: "gobie@example.com", name: "Gobie McDaniels", is_validated: true, member_level: 5},
     {password: bcrypt.hashSync('Testing1!!', 10), email: "jimbo@example.com", name: "Jimbo Fargo", is_validated: false, member_level: 1},
     {password: bcrypt.hashSync('Testing1!!', 10), email: "hardy@example.com", name: "Hardy Bridle", is_validated: true, member_level: 10},
