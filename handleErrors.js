2089959c7e71631f25c7f2073260b9088b50d2e6
Duncan Beevers <duncan@dweebd.com>
2023-04-28 22:33:11 -0700

    chore: Switch to pure-js version of bcrypt (#35)
    * chore: Switch to pure-js version of bcrypt
    
    The bcrypt module installs itself with node-gyp
    bcrypt compiled inside a Docker container may not have the same architecture as the host machine.
    In order to allow a smoother development experience, a pure javascript bcrypt implementation is used instead.
    Although optimized, I believe this implementation is still slower than the native version.
    However, as the algorithm is typically used with a tunable number of "rounds", some slowness is acceptable, and even desirable.
    
    * chore: Upgrade hapi dependencies
    
    hapi-related modules have been moved under the organization's namespace @hapi
    This PR updates all the deprecated hapi module references to the new namespaced versions.
    
    * chore: Consolidate event seeding
    
    003_events had an error
    Rather than correcting the error in that file, I rolled together 001 and 003 seeds, and fixed the error in the combined file

--- handleErrors.js
+++ handleErrors.js
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-const boom = require('boom');
+const boom = require('@hapi/boom');
 const debug = require('debug')('errors');
 const errors = require('./lib/error_types');
 
